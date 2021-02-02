18f6979142b64737451d41e88c0b9e45dbd72796
Stephen Wilson <swilson@SPEM907.local>
2021-02-02 18:55:50 -0500

    updates

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ USER=$(id -un)
 
 rm ~/.bash_profile
 ln -sv /Users/$USER/dotfiles/.bash_profile ~/
+ln -sv /Users/$USER/dotfiles/.zprofile ~/
 
 chmod +x /Users/$USER/dotfiles/scripts/diffmerge-svnmerge.sh
 chmod +x /Users/$USER/dotfiles/scripts/svn-diffwrap.sh
